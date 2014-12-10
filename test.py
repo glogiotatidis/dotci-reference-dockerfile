18d53b5f6f2c449a5f34505694ea3fd1eb1682c1
Giorgos Logiotatidis <seadog@sealabs.net>
2014-12-10 11:22:24 +0200

    Initial commit.

---
+++ test.py
@@ -0,0 +1,14 @@
+import unittest
+
+
+class MyTest(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(1 + 3, 4) 
+
+    def test_divide(self):
+        self.assertEqual(8 / 2, 4)
+
+
+if __name__ == '__main__':
+    unittest.main() 
